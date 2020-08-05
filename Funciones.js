e142ae312804490eebba89e650b34852ab37ad48
Jarriv <brajhan1@hotmail.com>
2020-08-05 13:12:45 -0500

    ajax added and finished

--- Funciones.js
+++ Funciones.js
@@ -43,3 +43,51 @@ function UserName(){
         User.value = '';
     }
 }
+$(document).ready(function () {
+    var datos;
+
+    $.ajax({
+        type: "GET",
+        url: "https://www.datos.gov.co/api/views/xdk5-pm3f/rows.json?accessType=DOWNLOAD",
+        data: "data",
+        dataType: "json",
+        success: function (response) {  
+            
+            datos = response.data;
+
+            d=[];
+        
+            for(var indice of datos)
+            {
+                if(d.indexOf(indice[10])==-1)
+                {
+                    $('#departamento').append
+                    (
+                        `<option value="${indice[10]}">${indice[10]}</option>`
+                    );
+
+                        d.push(indice[10]);
+                }
+            }
+
+            $('#departamento').change(function () { 
+        
+                valor=this.options[this.selectedIndex].value;
+                console.log(valor);
+                municipios=$("#ciudad");
+                $('#ciudad').html("");
+                $('#seleccionciudad').css('display', 'block');
+
+                for(var indice of datos)
+                {
+                    
+                    if(indice[10]==valor)
+                    
+                    municipios.append(
+                        `<option value="${indice[12]}">${indice[12]}</option>`
+                    );
+                } 
+            });
+        }
+    });
+});
